ea54c3fe5470577cf6928537faf00f1d2364cbe1
Pessi <pessiv@outlook.com>
2025-03-23 16:17:31 +0200

    -

--- scripts/main.js
+++ scripts/main.js
@@ -9,15 +9,15 @@ function createSections(sectionsarr) {
 //projects handler
 function projectsSection(divid) {
     const projectsec = document.getElementById(divid)
-    projectsec.innerHTML += "<h1>Projects</h1>"
+    projectsec.innerHTML += "<h1 class=\"text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl\">Projects</h1>"
     fetch("https://pessiv.github.io/data/projects.json")
     .then(response => {
         return response.json()
     })
     .then(data => {
         data.forEach(item => {
-            var htmlinsanity = "<div id=\""+item.repo+"\">";
-            htmlinsanity += "<h2>"+item.name+"</h2>";
+            var htmlinsanity = "<div class=\"grid h-screen place-items-center\" id=\""+item.repo+"\">";
+            htmlinsanity += "<h2 class=\"text-4xl font-extrabold\">"+item.name+"</h2>";
             htmlinsanity += "<p>"+item.description+"</p>";
             htmlinsanity += "<img src=\""+item.image+"\" width=\"400\" height=\"auto\">";
             htmlinsanity += "<p>Git repo:"+item.repo+"</p></div>";
