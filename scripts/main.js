1416cf98d8819e3e58201dd0cffe642066195a63
Pessi <pessiv@outlook.com>
2025-03-23 06:26:44 +0200

    -

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 document.title = "Portfolio Of Pessi Vilppolahti";
+//section divider
 const sectiondivider = document.getElementById("ssections")
 function createSections(sectionsarr) {
     sectionsarr.forEach(element => {
         sectiondivider.innerHTML += "<div id=\""+element+"\"></div>"
     });
+}
+//projects handler
+function projectsSection(divid) {
+    fetch("https://pessiv.github.io/data/projects.json")
+    .then(response => {
+        console.log(response)
+    })
 }
